6c502f8068ce671b65364475aaab264dfff08dfd
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-20 06:09:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,14 @@ class Dog
     @breed = breed
   end 
   
-  def Dog::create_table
-    
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql) 
+  end  
 end
