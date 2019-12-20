327ffe801ae158e2b244d154468474c58a34b6e1
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-20 06:31:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,11 @@ class Dog
     dog = DB[:conn].execute(sql, name, breed)
     if !dog.empty?
       dog_data = dog[0]
-    
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
     
     
     
