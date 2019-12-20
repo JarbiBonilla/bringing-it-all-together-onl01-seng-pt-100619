9ff61e305cd57a84da7d5198fba77b8ec0f14625
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-20 06:11:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,14 +8,5 @@ class Dog
     @breed = breed
   end 
   
-  def Dogs::create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    name TEXT
-    )
-    SQL
-    DB[:conn].execute(sql) 
-  end  
+  
 end
